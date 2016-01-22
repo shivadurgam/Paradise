ac6a93e2caa8249611013bfebf5a5cc69d282b50
shivadurgam <shivakrishnadurgam@gmail.com>
2016-01-22 07:57:00 +0530

    login added

--- route.js
+++ route.js
@@ -2,31 +2,82 @@ var express = require('express');
 var Parse = require('parse/node').Parse;
 var router = express.Router();
 var _ = require('lodash');
+var flash = require('connect-flash');
+var session = require('express-session');
+var cookieParser = require('cookie-parser');
+
 
 Parse.initialize('YMg0IjOSRw7FupnNTBZXs2LoiGxjcP4B5ruqOhYt', 'yfJ32G79PwYL0S1rjj8GsrjUCy0U7rQggAj742SA');
 var MenuItem = Parse.Object.extend('MenuItem');
 
 
 // define routes
-
 var toDoItems = [
   		{id: 1, firstName: "Shiva", lastName: "Durgam"},
   		{id: 2, firstName: "Krishna", lastName: "Reddy"},
   		{id: 3, firstName: "Durgam", lastName: "Chetty"}
   	];
-
 router.get('/', function (request, response) {
+  if(!request.session.visitcount){
+    request.session.visitcount = 1;
+  } else {
+    request.session.visitcount ++;
+  }
+  console.log(request.session);
   response.render('home', {
     title: 'Paradise Biryanai Point',
-  	items: toDoItems
+  	items: toDoItems,
   });
 });
 
 router.get('/index', function (request, response) {
   response.render('index', {
   	title: 'Paradise Biryani Point  ',
-  	items: toDoItems
+  	items: toDoItems,
   });
+});
+
+router.post('/signup', function(request, response){
+    var user = new Parse.User();
+    var username = request.body.username;
+    var password = request.body.password;
+
+    user.set("username", username);
+    user.set("password", password);
+
+    // other fields can be set just like with Parse.Object
+
+    user.signUp(null, {
+      success: function(user) {
+        // Hooray! Let them use the app now.
+        console.log("successfully registered");
+        request.flash('info', 'successfully registered')
+      },
+      error: function(user, error) {
+        // Show the error message somewhere and let the user try again.
+        console.log("Error " + error.code + " : " + error.message);
+        request.flash('error', 'signup failed')
+      }
+    });
+    response.render('home', {
+      locals: request.flash(),
+      title: 'Paradise Biryanai Point',
+    	items: toDoItems
+    });
+});
+
+router.post('/login', function(request, response){
+    var username = request.body.username;
+    var password = request.body.password;
+    Parse.User.logIn(username, password, {
+      success: function(user) {
+        console.log(username + " logged in successfully!! " );
+      },
+      error: function(user,error){
+        console.log("Error " + error.code + " : " + error.message);
+      }
+    });
+      response.redirect('/');
 });
 
 router.get('/addItem', function(request, response){
@@ -34,8 +85,8 @@ router.get('/addItem', function(request, response){
   menuItem.save({
     title: "Chicken Biryani",
     price: 12.5
-  }).then(function (response) { console.log(response); });
-  response.send('Hello World!');
+  }).then(function (response) { console.log(menuItem.price); });
+  response.send("hi");
 });
 
 router.get('/results', function(request, response){
--- server.js
+++ server.js
@@ -2,6 +2,10 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var path = require('path');
 var app = express();
+var flash = require('connect-flash');
+var session = require('express-session');
+var cookieParser = require('cookie-parser');
+
 
 
 //configure app
@@ -10,10 +14,12 @@ app.set('view engine', 'ejs');
 
 app.use(express.static('bower_components'));
 app.use(bodyParser());
+app.use(cookieParser());
+app.use(session({resave:true, saveUninitialized:true, secret:'dingdong'}));
+app.use(flash());
 
 // defining routes
 app.use(require('./route'));
-
 
 //start server
 var port = process.env.PORT || 3000;
